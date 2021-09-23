bba5b110d792e5543b4faa72e60a72b9da7e66a8
HeartKush <andresparra0905@gmail.com>
2021-09-23 10:11:27 -0500

    sidebar fix

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  item_selected:string = 'dashboard';
   constructor() { }
 
   ngOnInit(): void {
+
+    var items = document.querySelectorAll('.flex-column li a');
+    items.forEach((item)=>{
+        if ( item.getAttribute('data-current-item') == document.URL.replace('http://localhost:4200/','')){
+            item.classList.add("active2");
+            item.classList.remove("link-dark");        
+        }else{
+          item.classList.remove("active2");
+          item.classList.add("link-dark"); 
+        }
+    });
+
   }
 
 }
