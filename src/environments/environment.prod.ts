0974e50a1eb3159370def3dda7899d6002f750f8
HeartKush <andresparra0905@gmail.com>
2021-09-24 00:12:56 -0500

    firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyATY6ZvCET8WY3TM9h_oGdUAig9v8DYgRU",
+    authDomain: "useit-b41f4.firebaseapp.com",
+    projectId: "useit-b41f4",
+    storageBucket: "useit-b41f4.appspot.com",
+    messagingSenderId: "174774374342",
+    appId: "1:174774374342:web:91883154076c7c1dacecc7",
+    measurementId: "..."
+  }
 };
